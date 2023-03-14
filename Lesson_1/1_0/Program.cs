c91d73d308ffd187d20d9b625ecc6da8afdc7689
MikhailButyanov <mikhail@butyanov.ru>
2023-03-15 00:00:48 +0300

    Task 1_0 ver 01

--- Lesson_1/1_0/Program.cs
+++ Lesson_1/1_0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Моя первая суперическая программа
+Console.Write("Введите целое число: ");
+string s = Console.ReadLine()!; //Console.ReadLine() считывает только строки
+//! снимает предупреждения
+int num = int.Parse(s); // Преобразование string в int
+Console.Write("Квадрат вашего числа: ");
+Console.WriteLine(num * num); //Арифметические операции невыполняются со строками
