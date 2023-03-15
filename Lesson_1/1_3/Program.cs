cdf7f2375afbcc08214e52efe988f1ed2604aacd
MikhailButyanov <mikhail@butyanov.ru>
2023-03-15 16:48:20 +0300

    add programm 1_3

--- Lesson_1/1_3/Program.cs
+++ Lesson_1/1_3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. Напишите программу, которая на вход принимает одно число
+//    (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("Введите положительное целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = -a;
+while (b <= a)
+{
+    Console.Write($"{b} ");
+    b ++; //b = b + 1; 
+}
+Console.WriteLine("Отлично сделано!");
