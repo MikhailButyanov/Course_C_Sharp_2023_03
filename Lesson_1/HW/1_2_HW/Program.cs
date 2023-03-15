7d1d22f5367345bb001f1c4079452937be6f1c7a
MikhailButyanov <mikhail@butyanov.ru>
2023-03-15 21:48:01 +0300

    add programm in HW 1_2_HW

---
+++ Lesson_1/HW/1_2_HW/Program.cs
@@ -0,0 +1,34 @@
+﻿// Знакомство с языками программирования (семинары)
+// Урок 1. Знакомство с языком программирования С#
+// Задача 4: Напишите программу, 
+// которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Нахождение большего числа");
+Console.Write("Введите первое целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе целое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье целое число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine($"max={a}");
+    }
+    else
+    {
+        Console.WriteLine($"max={c}"); 
+    }
+}
+else if (b > c)
+{
+    Console.WriteLine($"max={b}");
+}
+else
+{
+    Console.WriteLine($"max={c}");  
+}
+Console.WriteLine("Хорошая работа!");
