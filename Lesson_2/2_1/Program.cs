9a79de9acc4d92946215c1880cb52f4ee806ff90
MikhailButyanov <mikhail@butyanov.ru>
2023-03-17 01:31:37 +0300

    Task 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1
+// принимаем на вход трёхзначное число, выводим последнюю цифру этого числа
+int TakeNum(int num)
+{ //тело функции
+    Console.WriteLine(num);
+    return num % 10;
+} //тело функции
+
+
+int result = TakeNum(new Random().Next(100, 1000));
+// int num = new Random().Next(100, 1000); //выдаёт от 100 до 999
+
+Console.WriteLine(result);
+//Console.WriteLine(num % 10);
+// shift alt A - комментировать выделенный блок
