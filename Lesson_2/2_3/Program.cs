6063ec4c06db456f5392f537b7a142186c9d6e5d
MikhailButyanov <mikhail@butyanov.ru>
2023-03-17 16:47:29 +0300

    Task 2_2 programm

--- Lesson_2/2_3/Program.cs
+++ Lesson_2/2_3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Язык C#. Задача 3: написать программу.
+// Вводим два числа
+// Функция принимает на вход через эти числа и 
+// выводит текст является ли второе числа кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// "/ 10" убираем последнюю цифру, целочисленное деление, 
+// «% 10» получение последней цифры, остаток от деления, кратно
+// shift alt A - комментировать выделенный блок
+
+void CheckNum(int num1, int num2)
+{
+    if (num1 % num2 == 0)
+        //Console.WriteLine(num2 + " кратно " + num1);
+        Console.WriteLine($"{num2} кратно {num1}");
+    else
+        Console.WriteLine(num2 + " не кратно " + num1 + ", остаток от деления: " + num1 % num2);
+        //Console.WriteLine($"{num2} не кратно {num1}, остаток от деления: {num2 % num1}");
+}
+
+Console.WriteLine("Введите два числа:");
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+CheckNum(num1, num2);
+
+
