376bba872e088c3c9fbd0feef3ffb1b7518613da
MikhailButyanov <mikhail@butyanov.ru>
2023-03-17 14:41:24 +0300

    Task 2_2 begin_01 programm

--- Lesson_2/2_2/Program.cs
+++ Lesson_2/2_2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2
+// вывод случайного числа из [10, 99], выводим наибольшую цифру этого числа
+int TakeNum(int num)
+{ //тело функции
+    Console.WriteLine(num);
+    return num % 10;
+} //тело функции
+
+
+int result = TakeNum(new Random().Next(100, 1000));
+// int num = new Random().Next(100, 1000); //выдаёт от 100 до 999
+
+Console.WriteLine(result);
+//Console.WriteLine(num % 10);
+// shift alt A - комментировать выделенный блок
